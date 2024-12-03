691543e8c3cba1a88bb251ddb5d49dbc15754d9d
roibenyaacov <roi.benyaacov@post.runi.ac.il>
2024-12-03 18:07:56 +0200

    Update Primes.java

--- Primes.java
+++ Primes.java
@@ -1,5 +1,34 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
-    }
-}
+        int n = Integer.parseInt(args[0]);
+        boolean[] arrPrime = new boolean[n + 1 ];
+        double sqrt = Math.sqrt(n);
+        int p = 2; 
+
+        for (int i = 2; i < n; i++) {
+            arrPrime [i] = true; 
+        }
+        while (p <= sqrt) { 
+            if (arrPrime[p]) {
+                int multiple = p * p;
+                while (multiple <= n) { 
+                    arrPrime [multiple] = false ; 
+                    multiple += p; 
+                } 
+            } 
+            p++;
+        } 
+        int count = 0;
+        for (int i = 2; i < n ; i++) {
+            if (arrPrime [i]) {
+            System.out.println(i);
+            count++;
+            }
+        }
+        double percent = ((double)count / (double)n) * 100 ;
+            System.out.println(" There are " + count + " primes between 2 and " + n + "(" + (int) percent + "% are primes" + ")");
+             
+        }
+    
+ }
+        
